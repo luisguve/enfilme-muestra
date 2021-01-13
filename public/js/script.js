1f307ef4de8774cf6bf8bae53dcaafb5c7c5bde2
Luis G. Villegas <luisguveal@gmail.com>
2021-01-13 10:00:02 -0430

    Defined rules for responsiveness

--- public/js/script.js
+++ public/js/script.js
@@ -8,13 +8,35 @@ $(document).ready(function(){
 		autoplay: false
 	});
 	$('.slider-nav').slick({
-		slidesToShow: 3,
-		slidesToScroll: 3,
+		slidesToShow: 4,
+		slidesToScroll: 4,
 		asNavFor: '.slider-for',
 		dots: false,
 		centerMode: false,
 		focusOnSelect: true,
 		prevArrow: $('.prev-button'),
-		nextArrow: $('.next-button')
+		nextArrow: $('.next-button'),
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 576,
+				settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 400,
+				settings: "unslick"
+			}
+		]
 	});
 });

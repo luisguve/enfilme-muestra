0c02e6064618dff4b172ff72057e60dd50ef7680
Luis G. Villegas <luisguveal@gmail.com>
2021-01-17 16:31:00 -0430

    Updated list of estrenos

--- public/js/script.js
+++ public/js/script.js
@@ -48,44 +48,44 @@ $(document).ready(function(){
 	// Setup event listeners.
 	const estrenosContents = [
 		{
-			img: "public/img/news/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
+			img: "public/img/en_cartelera/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
 			title: "vaquero del mediodia",
 			desc: "Una luz que se apaga en el camino."
 		},
 		{
-			img: "public/img/news/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
-			title: "vaquero del mediodia 2",
-			desc: "Una luz que se apaga en el camino."
+			img: "public/img/en_cartelera/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
+			title: "to the ends of the earth",
+			desc: "Yoko (Atsuko Maeda) es una joven reportera japonesa que, debido a una misión de trabajo, viaja a Uzbekistán donde se filma un reportaje cultural sobre las costumbres, tradiciones y atractivos turísticos de aquel país."
 		},
 		{
-			img: "public/img/news/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
-			title: "vaquero del mediodia 3",
-			desc: "Una luz que se apaga en el camino."
+			img: "public/img/en_cartelera/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
+			title: "ceniza negra",
+			desc: "Selva (Smashleen Gutiérrez), una niña de 13 años, vive con su abuelo Tata (Humberto Samuels) y una figura maternal, Elena (Hortensia Smith), en una choza escondida en el corazón de la jungla costarricense."
 		},
 		{
-			img: "public/img/news/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
-			title: "vaquero del mediodia 4",
-			desc: "Una luz que se apaga en el camino."
+			img: "public/img/en_cartelera/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
+			title: "Dawson City: Frozen Time",
+			desc: "Con casi el 75% del cine mudo estadounidense perdido debido a la naturaleza autodestructiva de las primeras películas de nitrato, una gran parte de su historia ha permanecido fuera de nuestro alcance. Esa es parte de la razón por la que Dawson City: Frozen Time (2016) nos lleva a un viaje tan inmersivo, ya que trae a la pantalla una colección de imágenes que alguna vez se asumió perdida para siempre."
 		},
 		{
-			img: "public/img/news/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
-			title: "vaquero del mediodia 5",
-			desc: "Una luz que se apaga en el camino."
+			img: "public/img/en_cartelera/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
+			title: "una película dramática",
+			desc: "En su más reciente trabajo cinematográfico, Un Film Dramatique (2019), el artista y cineasta Éric Baudelaire se ha despojado de la etiqueta de “autor de la obra” para explorar el potencial de la colaboración con estudiantes de la escuela secundaria Dora Maar, ubicada en el suburbio parisino de Saint-Denis."
 		},
 		{
-			img: "public/img/news/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
-			title: "vaquero del mediodia 6",
-			desc: "Una luz que se apaga en el camino."
+			img: "public/img/en_cartelera/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
+			title: "farewell amor",
+			desc: "Walter (Ntare Guma Mbaho Mwine), un ciudadano originario de Angola, ha estado viviendo en Nueva York durante 17 años. Ahora, su esposa, Esther (Zainab Jah), y su hija adolescente, Sylvia (Jayme Lawson), a ninguna de las cuales ha visto en todo ese tiempo, se unirán a él en la Gran Manzana."
 		},
 		{
-			img: "public/img/news/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
-			title: "vaquero del mediodia 7",
-			desc: "Una luz que se apaga en el camino."
+			img: "public/img/en_cartelera/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
+			title: "mis escenas de lucha",
+			desc: "Una joven desconcertada (Sara Forestier), que está de visita en la Francia rural para ayudar a su hermana a manejar la propiedad de su padre, está enfurecida porque él no le dio el piano que le prometió que heredaría."
 		},
 		{
-			img: "public/img/news/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
-			title: "vaquero del mediodia 8",
-			desc: "Una luz que se apaga en el camino."
+			img: "public/img/en_cartelera/vaquero_del_mediodia_Enfilme_6c240_340_170.jpg",
+			title: "Elegy to the Visitor from the Revolution",
+			desc: "Ambientado en Filipinas durante la actualidad, una prostituta (Sigrid Bernardo) espera pacientemente a un cliente más. Un músico (Lav Diaz) interpreta varias melodías y canciones sin un destinatario específico. Un grupo de delincuentes (Dante Perez, Evelyn Vargas y Joel Ferrer) se prepara para un atraco."
 		}
 	];
 	const estrenosList = document.querySelectorAll(".estreno-desc");
